0385f9452ac1c682b15698929c65f91ad94584ab
Eduardo Gardin <eduardoj.gardin@gmail.com>
2018-02-01 15:32:54 -0200

    Initial commit

---
+++ src/introducao/historia.js
@@ -0,0 +1,23 @@
+
+export const surgimento = {
+    ano: 1995,    
+    descricao: 'Javascript',
+    propositos: [
+        'Adicionar programas nas páginas web',
+        'Comportamento para páginas web estáticas'
+    ],
+    observacoes: [
+        'NÃO É JAVA',
+        'ECMAScript => Padronização',
+        'ES5 Lançado em 2009',
+        'ES6 Lançado em 20015'
+    ]
+};
+
+export const caracteristicas = [
+    'Interpretada',
+    'Case sensitive',
+    'Regra de nomenclatura das variáveis do C',
+    'Dinâmica',
+    'Hoisting dos blocos'
+];
---
+++ src/introducao/historia.test.js
@@ -0,0 +1,14 @@
+const { surgimento } = require('./historia');
+
+describe('História do Javascript', () => {
+
+    it('surgimento ', () => {
+
+        expect(surgimento.ano).toEqual(1995);
+
+    });    
+
+    it('características', () => {
+
+    });
+});
