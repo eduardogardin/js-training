63620271fbc50e3d087705c15de9910566cd77e4
Eduardo Gardin <eduardoj.gardin@gmail.com>
2018-03-09 16:01:04 -0300

    Mais conteúdo

---
+++ src/11-sync-async/sync-async.js
@@ -0,0 +1,19 @@
+export const syncObject = {
+    definicoes: [
+        'Uma coisa de cada vez',
+        'Bloqueante',
+        'Aplicação congela',
+        'Uso de threads para paralelizar (Gerenciamento complexo)',
+        'exemplo-1'
+    ],
+
+};
+
+export const asyncObject = {
+    definicoes: [
+        'Várias coisas acontecendo ao mesmo tempo',
+        'Não bloqueante',
+        'Inicia várias funções ao mesmo tempo, e você é notificado na conclusão da execução',
+        'exemplo-1'
+    ]
+};
---
+++ src/12-callbacks/callbacks.js
@@ -0,0 +1,16 @@
+
+export const callbackDef = {
+    definicoes: [
+        'Função que será chamado quando algo vai acontecer',
+        '#Chamada não atendida',
+        ''
+    ],
+    vantagens: [
+        '????'
+    ],
+    desvantagens: [
+        'Não é garantido por tipo',
+        'Encadeamento complexo e difícil de ler e manter',
+        ''
+    ]
+};
